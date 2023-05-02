a902b511f68bba5117b1a9c29a906d368bcbb845
kiran94 <kiran_patel94@hotmail.com>
2023-05-02 21:27:26 +0100

    feat(collector): implemented github

---
+++ prfiesta/collectors/github.py
@@ -0,0 +1,130 @@
+import logging
+from datetime import datetime
+
+import pandas as pd
+from github import Github
+from rich.spinner import Spinner
+
+from prfiesta import SPINNER_STYLE
+from prfiesta.environment import GitHubEnvironment
+
+logger = logging.getLogger(__name__)
+
+
+class GitHubCollector:
+
+    def __init__(self, **kwargs) -> None:
+
+        environment = GitHubEnvironment()
+        token = kwargs.get("token") or environment.get_token()
+        self._url = kwargs.get("url") or environment.get_url()
+
+        self._github = Github(token, base_url=self._url)
+        self._spinner: Spinner = kwargs.get("spinner")
+
+        self._sort_column = ["updated_at"]
+        self._drop_columns = [
+            "labels_url",
+            "comments_url",
+            "events_url",
+            "node_id",
+            "performed_via_github_app",
+            "active_lock_reason",
+        ]
+        self._move_to_end_columns = [
+            "url",
+            "repository_url",
+            "html_url",
+            "timeline_url",
+        ]
+        self._datetime_columns = [
+            "created_at",
+            "updated_at",
+            "closed_at",
+            "milestone.created_at",
+            "milestone.updated_at",
+            "milestone.due_on",
+            "milestone.closed_at",
+        ]
+
+
+    def collect(self, users: list[str], after: datetime | None= None, before: datetime | None = None) -> pd.DataFrame:
+
+        query = self._construct_query(users, after, before)
+
+        self._update_spinner(f"Searching {self._url} with[bold blue] {query}")
+        pulls = self._github.search_issues(query=query)
+
+        pull_request_data: list[dict] = []
+        for pr in pulls:
+            pull_request_data.append(pr.__dict__["_rawData"])
+
+        if not pull_request_data:
+            logger.warning("Did not find any results for this search criteria!")
+            return pd.DataFrame()
+
+        self._update_spinner("Post Processing")
+        pr_frame = pd.json_normalize(pull_request_data)
+
+        pr_frame = pr_frame.drop(columns=self._drop_columns, errors="ignore")
+        pr_frame = pr_frame.sort_values(by=self._sort_column, ascending=False)
+        pr_frame = self._parse_datetime_columns(pr_frame)
+        pr_frame["repository_name"] = pr_frame["repository_url"].str.extract(r"(.*)\/repos\/(?P<repository_name>(.*))")["repository_name"]
+        pr_frame = self._move_column_to_end(pr_frame)
+
+        return pr_frame
+
+
+    @staticmethod
+    def _construct_query(users: list[str], after: datetime | None= None, before: datetime | None = None) -> str:
+        """
+        Constructs a GitHub Search Query
+        that returns pull requests made by the passed users.
+
+        Examples
+        --------
+            type:pr author:user1
+            type:pr author:user2 updated:<=2021-01-01
+            type:pr author:user1 author:user2 updated:2021-01-01..2021-03-01
+
+        All dates are inclusive.
+        See GitHub Docs for full optons https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests
+        """
+        query: list[str] = []
+
+        query.append("type:pr")
+
+        for u in users:
+            query.append("author:" + u)
+
+        if before and after:
+            query.append(f"updated:{after.strftime('%Y-%m-%d')}..{before.strftime('%Y-%m-%d')}")
+        elif before:
+            query.append(f"updated:<={before.strftime('%Y-%m-%d')}")
+        elif after:
+            query.append(f"updated:>={after.strftime('%Y-%m-%d')}")
+
+        return " ".join(query)
+
+    def _move_column_to_end(self, df):
+        for col in self._move_to_end_columns:
+            df.insert(len(df.columns)-1, col, df.pop(col))
+            df.drop(columns=col)
+
+        return df
+
+    def _parse_datetime_columns(self, df) -> pd.DataFrame:
+        for col in self._datetime_columns:
+            df[col] = pd.to_datetime(df[col], errors="ignore")
+        return df
+
+
+    def _update_spinner(self, message, style=SPINNER_STYLE):
+        if self._spinner:
+            self._spinner.update(text=message, style=style)
+
+
+
+if __name__ == "__main__":  # pragma: nocover
+    g = GitHubCollector()
+    print(g._construct_query(["kiran94", "hello"], datetime(2021, 1, 1), datetime(2021, 3, 1)))
---
+++ tests/collectors/test_github.py
@@ -0,0 +1,128 @@
+from datetime import datetime
+from unittest.mock import Mock, call, patch
+
+import pytest
+
+from prfiesta.collectors.github import GitHubCollector
+
+_mock_issue1 = Mock()
+_mock_issue1.__dict__ = {
+        "_rawData": {
+            "url": "my_url",
+            "repository_url": "https://api.github.com/repos/user/repo",
+            "html_url": "",
+            "timeline_url": "",
+            "labels_url": "",
+            "comments_url": "",
+            "events_url": "",
+            "node_id": "",
+            "performed_via_github_app": "",
+            "active_lock_reason": "",
+            "created_at": '2021-01-01',
+            "updated_at": datetime(2021, 1, 1),
+            "closed_at": datetime(2021, 1, 1),
+            "milestone.created_at": '2020-03-04',
+            "milestone.updated_at": datetime(2021, 1, 1),
+            "milestone.due_on": datetime(2021, 1, 1),
+            "milestone.closed_at": datetime(2021, 1, 1),
+        },
+}
+
+_mock_issue2 = Mock()
+_mock_issue2.__dict__ = {
+        "_rawData": {
+            "url": "my_url",
+            "repository_url": "https://api.github.com/repos/user1/repo",
+            "html_url": "",
+            "timeline_url": "",
+            "labels_url": "",
+            "comments_url": "",
+            "events_url": "",
+            "node_id": "",
+            "performed_via_github_app": "",
+            "active_lock_reason": "",
+            "created_at": datetime(2021, 1, 1),
+            "updated_at": datetime(2021, 1, 2),
+            "closed_at": datetime(2021, 1, 1),
+            "milestone.created_at": datetime(2021, 1, 1),
+            "milestone.updated_at": datetime(2021, 1, 1),
+            "milestone.due_on": datetime(2021, 1, 1),
+            "milestone.closed_at": datetime(2021, 1, 1),
+        },
+}
+
+
+
+
+@pytest.mark.parametrize(("collect_params", "github_issues", "expected_github_query"), [
+    (
+        {"users": ["user1", "user2"]},
+        [],
+        None,
+    ),
+    (
+        {"users": ["user1", "user2"]},
+        [ _mock_issue1, _mock_issue2 ],
+        "type:pr author:user1 author:user2",
+    ),
+    (
+        {"users": ["user1", "user2"], "after": datetime(2009, 1, 1)},
+        [ _mock_issue1, _mock_issue2 ],
+        "type:pr author:user1 author:user2 updated:>=2009-01-01",
+    ),
+    (
+        {"users": ["user1", "user2"], "before": datetime(2009, 1, 1)},
+        [ _mock_issue1, _mock_issue2 ],
+        "type:pr author:user1 author:user2 updated:<=2009-01-01",
+    ),
+    (
+        {"users": ["user1", "user2"], "after": datetime(2009, 1, 1),  "before": datetime(2010, 1, 1)},
+        [ _mock_issue1, _mock_issue2 ],
+        "type:pr author:user1 author:user2 updated:2009-01-01..2010-01-01",
+    ),
+])
+@patch("prfiesta.collectors.github.Github")
+def test_collect(
+        mock_github: Mock,
+        collect_params: dict,
+        github_issues: list[Mock],
+        expected_github_query: str,
+    ):
+
+    spinner_mock = Mock()
+    collector_params = {
+        "token": "dummy_token",
+        "url": "dummy_url",
+        "spinner": spinner_mock,
+    }
+
+    mock_github.return_value.search_issues.return_value = github_issues
+
+    gc = GitHubCollector(**collector_params)
+    returned = gc.collect(**collect_params)
+
+    assert mock_github.call_args_list == [call("dummy_token", base_url="dummy_url")]
+    assert gc._spinner == collector_params["spinner"]
+    assert spinner_mock.update.called
+
+    if not github_issues:
+        assert returned.empty
+        return
+
+    assert mock_github.return_value.search_issues.call_args_list == [call(query=expected_github_query)]
+
+    # Ensure the rows and columns came as expected
+    assert returned.shape == (2, 12)
+
+    # Ensure none of the drop columns came through
+    assert not set(gc._drop_columns).intersection(set(returned.columns.tolist()))
+
+    # Ensure that updated at is orderered in decreasing order
+    assert returned['updated_at'].is_monotonic_decreasing
+
+    # Ensure the all the input repos came through
+    assert set(returned["repository_name"].unique().tolist()) == {"user/repo", "user1/repo"}
+
+    # Ensure all the datetime columns were converted
+    for col in returned[gc._datetime_columns].columns.tolist():
+        assert str(returned[col].dtype) == 'datetime64[ns]', f'{col} was not a datetime column'
