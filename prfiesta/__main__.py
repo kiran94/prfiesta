9e3f1e10842eafc143fb9b6d66e5e1a8058a37d3
kiran94 <kiran_patel94@hotmail.com>
2023-05-03 22:06:46 +0100

    feat(main): implement

---
+++ prfiesta/__main__.py
@@ -0,0 +1,50 @@
+import logging
+from datetime import datetime
+
+import click
+from rich.live import Live
+from rich.spinner import Spinner
+from rich.text import Text
+
+from prfiesta import SPINNER_STYLE
+from prfiesta.collectors.github import GitHubCollector
+from prfiesta.environment import GitHubEnvironment
+from prfiesta.output import output_frame
+
+logger = logging.getLogger(__name__)
+
+github_environment = GitHubEnvironment()
+
+
+@click.command()
+@click.option('-u', '--users', required=True, multiple=True, help='The GitHub Users to search for. Can be multiple (space delimited)')
+@click.option('-t', '--token', required=True, default=github_environment.get_token(), help='The Authentication token to use')
+@click.option('-x', '--url', default=github_environment.get_url(), help='The URL of the Git provider to use')
+@click.option('-o', '--output_type', type=click.Choice(['csv', 'parquet']), default='csv', help='The output format')
+@click.option('--after', type=click.DateTime(formats=['%Y-%m-%d']), help='Only search for pull requests after this date e.g 2023-01-01')
+@click.option('--before', type=click.DateTime(formats=['%Y-%m-%d']), help='Only search for pull requests before this date e.g 2023-04-30')
+def main(**kwargs) -> None:
+
+    users: tuple[str] = kwargs.get('users')
+    token: str = kwargs.get('token')
+    url: str = kwargs.get('url')
+    output_type: str = kwargs.get('output_type')
+    before: datetime = kwargs.get('before')
+    after: datetime = kwargs.get('after')
+
+    logger.info('[bold green]Pull Request Fiesta 🦜🥳')
+
+    spinner = Spinner('dots', text=Text('Loading', style=SPINNER_STYLE))
+
+    with Live(spinner, refresh_per_second=20, transient=True):
+
+        collector = GitHubCollector(token=token, url=url, spinner=spinner)
+        pr_frame = collector.collect(users, after=after, before=before)
+
+        logger.info('Found [bold green]%s[/bold green] pull requests!', pr_frame.shape[0])
+
+        if not pr_frame.empty:
+            output_frame(pr_frame, output_type, spinner=spinner)
+
+if __name__ == '__main__':  # pragma: nocover
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from unittest.mock import ANY, Mock, call, patch
+
+import pandas as pd
+import pytest
+from click.testing import CliRunner
+
+from prfiesta.__main__ import main
+
+FAILURE_CODE = 2
+
+
+def test_main_missing_users() -> None:
+    runner = CliRunner()
+    result = runner.invoke(main, [''])
+
+    assert "Missing option '-u' / '--users'" in result.output
+    assert result.exit_code == FAILURE_CODE
+
+@pytest.mark.parametrize(('params', 'expected_collect_params', 'collect_response', 'expected_output_type'), [
+    (
+        ['--users', 'test_user'],
+        [call(('test_user',), after=None, before=None)],
+        pd.DataFrame(),
+        'csv',
+    ),
+    (
+        ['--users', 'test_user', '--after', '2020-01-01'],
+        [call(('test_user',), after=datetime(2020, 1, 1), before=None)],
+        pd.DataFrame(),
+        'csv',
+    ),
+    (
+        ['--users', 'test_user', '--before', '2020-01-01'],
+        [call(('test_user',), before=datetime(2020, 1, 1), after=None)],
+        pd.DataFrame(),
+        'csv',
+    ),
+    (
+        ['--users', 'test_user', '--before', '2020-01-01', '--after', '2009-01-01'],
+        [call(('test_user',), before=datetime(2020, 1, 1), after=datetime(2009, 1, 1))],
+        pd.DataFrame(),
+        'csv',
+    ),
+    (
+        ['--users', 'test_user'],
+        [call(('test_user',), after=None, before=None)],
+        pd.DataFrame(
+                data=[(1, 2, 3)],
+                columns=['col1', 'col2', 'col3'],
+        ),
+        'csv',
+    ),
+    (
+        ['--users', 'test_user', '--output_type', 'parquet'],
+        [call(('test_user',), after=None, before=None)],
+        pd.DataFrame(
+                data=[(1, 2, 3)],
+                columns=['col1', 'col2', 'col3'],
+        ),
+        'parquet',
+    ),
+])
+@patch('prfiesta.__main__.Spinner')
+@patch('prfiesta.__main__.Live')
+@patch('prfiesta.__main__.GitHubCollector')
+@patch('prfiesta.__main__.output_frame')
+def test_main(
+        mock_output_frame: Mock,
+        mock_collector: Mock,
+        mock_live: Mock,
+        mock_spinner: Mock,
+        params: list[str],
+        expected_collect_params: list,
+        collect_response: pd.DataFrame,
+        expected_output_type: str,
+    ) -> None:
+
+    mock_collector.return_value.collect.return_value = collect_response
+    runner = CliRunner()
+    result = runner.invoke(main, params)
+
+    assert mock_live.called
+    assert mock_spinner.called
+
+    assert mock_collector.call_args_list == [call(token=ANY, url='https://api.github.com', spinner=mock_spinner.return_value)]
+    assert mock_collector.return_value.collect.call_args_list == expected_collect_params
+
+    if not collect_response.empty:
+        assert mock_output_frame.call_args_list == [call(collect_response, expected_output_type, spinner=mock_spinner.return_value)]
+
+    assert result.exit_code == 0
