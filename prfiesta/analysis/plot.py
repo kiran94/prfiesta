1d58dbb07d3b05a28570e66793157df17c50d0c0
kiran94 <kiran_patel94@hotmail.com>
2023-05-14 11:04:18 +0100

    feat(analysis): implemented overall timeline distribution

--- prfiesta/analysis/plot.py
+++ prfiesta/analysis/plot.py
@@ -24,3 +24,35 @@ def plot_state_distribution(data: pd.DataFrame, **kwargs) -> Union[plt.Figure, p
 
     return sns.histplot(data, x='state', hue=hue, ax=ax, palette=palette)
 
+def plot_overall_timeline(data: pd.DataFrame, **kwargs) -> Union[plt.Figure, plt.Axes, pd.DataFrame]:
+
+    ax: Optional[Axes] = kwargs.get('ax')
+    palette: Optional[str] = kwargs.get('palette')
+    title: Optional[str] = kwargs.get('title', 'Overall Contributions')
+    hue: Optional[str] = kwargs.get('hue', 'month')
+
+    temp = data.copy()
+
+    temp['month'] = temp['created_at'].dt.month_name()
+    temp['year'] = temp['created_at'].dt.year.astype(str)
+
+    temp = temp.groupby(['month', 'year'])['id'].count()
+    temp = temp.reset_index()
+
+    # X Axis Ordering (Year)
+    x_order = temp['year'].unique().tolist()
+    x_order = [int(x) for x in x_order]
+    x_order.sort()
+    x_order = [str(x) for x in x_order]
+
+    # Hue Ordering (Months)
+    sorted_months = sorted(_months, key=lambda x: list(calendar.month_name).index(x))
+
+    p = sns.barplot(temp, x='year', y='id', hue=hue, ax=ax, order=x_order, hue_order=sorted_months, palette=palette)
+
+    if ax:
+        ax.set_title(title)
+        ax.legend(loc='upper left')
+
+    return p
+
--- tests/analysis/test_plot.py
+++ tests/analysis/test_plot.py
@@ -1,15 +1,21 @@
+from datetime import datetime
 from typing import Dict
-from unittest.mock import Mock, call, patch
+from unittest.mock import ANY, Mock, call, patch
 
 import pandas as pd
 import pytest
 
-from prfiesta.analysis.plot import plot_state_distribution
+from prfiesta.analysis.plot import _months, plot_overall_timeline, plot_state_distribution
 
 _mock_state_distribution_axis = Mock()
+_mock_overall_timeline_axis = Mock()
 
 
 @pytest.mark.parametrize(('options', 'expected_options'), [
+    pytest.param(
+        {},
+        { 'x': 'state', 'hue': 'repository_name', 'ax': None, 'palette': None },
+        id='default'),
     pytest.param(
         {'ax': _mock_state_distribution_axis},
         { 'x': 'state', 'hue': 'repository_name', 'ax': _mock_state_distribution_axis, 'palette': None },
@@ -48,3 +54,55 @@ def test_plot_state_distribution(mock_seaborn: Mock, options: Dict, expected_opt
     assert mock_seaborn.histplot.call_args_list == [call(data, **expected_options)]
 
     _mock_state_distribution_axis.reset_mock()
+
+
+@pytest.mark.parametrize(('options', 'expected_options'), [
+   pytest.param(
+        {},
+        {'x':'year', 'y':'id', 'hue':'month', 'ax': None, 'order':['2021', '2022'], 'hue_order':_months, 'palette':None},
+        id='default',
+    ),
+   pytest.param(
+        {'ax': _mock_overall_timeline_axis },
+        {'x':'year', 'y':'id', 'hue':'month', 'ax': _mock_overall_timeline_axis, 'order':['2021', '2022'], 'hue_order':_months, 'palette':None},
+        id='with_custom_axis',
+    ),
+   pytest.param(
+        {'palette': 'tab10'},
+        {'x':'year', 'y':'id', 'hue':'month', 'ax': None, 'order':['2021', '2022'], 'hue_order':_months, 'palette':'tab10'},
+        id='with_custom_pallete',
+    ),
+   pytest.param(
+        { 'ax': _mock_overall_timeline_axis, 'title': 'my_title'},
+        {'x':'year', 'y':'id', 'hue':'month', 'ax': _mock_overall_timeline_axis, 'order':['2021', '2022'], 'hue_order':_months, 'palette':None},
+        id='with_custom_title',
+    ),
+   pytest.param(
+        {'hue': 'custom_field'},
+        {'x':'year', 'y':'id', 'hue':'custom_field', 'ax': None, 'order':['2021', '2022'], 'hue_order':_months, 'palette':None},
+        id='with_custom_hue',
+    ),
+])
+@patch('prfiesta.analysis.plot.sns')
+def test_plot_overall_timeline(mock_seaborn: Mock, options: Dict, expected_options: Dict) -> None:
+
+    mock_barplot = Mock()
+    mock_seaborn.barplot.return_value = mock_barplot
+
+    data = pd.DataFrame(data={
+        'created_at': [datetime(2021, 1, 1), datetime(2021, 2, 1), datetime(2022, 4, 14)],
+        'id': ['id1', 'id2', 'id3'],
+    })
+
+    result = plot_overall_timeline(data, **options)
+
+    assert mock_barplot == result
+    assert mock_seaborn.barplot.call_args_list == [call(ANY, **expected_options)]
+
+    if ('title' in options) and (options['title']):
+        assert _mock_overall_timeline_axis.set_title.call_args_list == [call(options['title'])]
+
+    _mock_overall_timeline_axis.reset_mock()
+
+
+
