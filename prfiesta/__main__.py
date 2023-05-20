d317ad058e10952e926bb73c9db607b8bdc556b3
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2023-05-20 14:04:43 +0100

    docs(main): add analysis link to script output (#20)

--- prfiesta/__main__.py
+++ prfiesta/__main__.py
@@ -48,5 +48,7 @@ def main(**kwargs) -> None:
         if not pr_frame.empty:
             output_frame(pr_frame, output_type, spinner=spinner, output_name=output)
 
+        logger.info('Time to analyze 🔎 See https://github.com/kiran94/prfiesta/blob/main/docs/analysis.md for some inspiration!')
+
 if __name__ == '__main__':  # pragma: nocover
     main()
